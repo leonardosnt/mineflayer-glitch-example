8c3a9b21d4173a3648f1313c49fa38ff992148ee
Leonardo Santos <devleeo@outlook.com>
2017-05-02 17:39:14 -0300

    some changes

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const mineflayer = require('mineflayer');
 const options = {
   host: process.env.SERVER_HOST,      // Server hostname
   port: process.env.SERVER_PORT,      // Server port
-  username: process.env.BOT_USERNAME  // Bot username
+  username: process.env.BOT_USERNAME, // Bot username,
+  version: process.env.BOT_VERSION    // Minecraft version
 };
 
 // You must specify host & port
 if (!options.host || !options.port) {
   console.log('Please, specify SERVER_HOST & SERVER_PORT');
-  return;
+  process.exit(1)
 }
 
 // You must specify password if you want join in online-mode servers.
@@ -21,7 +22,7 @@ if (process.env.PASSWORD) {
   options.password = process.env.PASSWORD;
 }
 
-let bot = mineflayer.createBot(options);
+const bot = mineflayer.createBot(options);
 
 bot.on('login', () => {
   console.log('Bot connected :D');
@@ -29,18 +30,7 @@ bot.on('login', () => {
 
 // Print chat messages on Console.
 bot.on('message', (message) => {
-  // Parse JSON message
-  if (message.json.extra) {
-    let parsedMsg = '';
-    message.json.extra.forEach(c => {
-      if (typeof c === 'string') {
-        parsedMsg += c;
-      } else {
-        parsedMsg += c.text;
-      }
-    });
-    console.log(`[CHAT] ${parsedMsg}`);
-  }
+  console.log(`[CHAT] ${message.toString()}`);
 });
 
 // Bot logic here
